17179ebcb2a3ae71ed65b728bcd8ab40e4f2ff95
Adarsh R <adarshraj1003@gmail.com>
2020-06-23 12:39:40 +0530

    py prog to open maps from the address in the clipboard

---
+++ maploc.py
@@ -0,0 +1,8 @@
+#opening google maps and locate trivandrum
+
+import pyperclip, webbrowser
+
+# address = pyperclip.paste()
+address = 'trivandrum'
+
+webbrowser.open('https://www.google.com/maps/place/'+address)
